557b08a449f4ee213410d9e70b683cfed0a46f2b
srushti <srushtiwaingade@gmail.com>
2026-08-10 00:13:15 +0530

    Initial spring boot setup with sql and docker

---
+++ src/test/java/com/example/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+    @Test
+    void contextLoads(){}
+}
